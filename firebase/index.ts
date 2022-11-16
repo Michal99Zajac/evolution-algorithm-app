6e9b63b297424ea5791c31641085332e52d889e4
Michal99Zajac <michal.99.zajac@gmail.com>
2022-11-16 22:52:57 +0100

    feat(env): fill all firestore variables

--- firebase/index.ts
+++ firebase/index.ts
@@ -1,16 +1,21 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
-import { FIREBASE_API_KEY } from '@/config/env'
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_APP_ID,
+  FIREBASE_APP_NAME,
+  FIREBASE_SENDER_ID,
+} from '@/config/env'
 
 // Firebase configuration
 const firebaseConfig = {
   apiKey: FIREBASE_API_KEY,
-  authDomain: 'evolution-algorithm-oe.firebaseapp.com',
-  projectId: 'evolution-algorithm-oe',
-  storageBucket: 'evolution-algorithm-oe.appspot.com',
-  messagingSenderId: '196763402196',
-  appId: '1:196763402196:web:29b5e7f6690259f2ce6041',
+  authDomain: `${FIREBASE_APP_NAME}.firebaseapp.com`,
+  projectId: FIREBASE_APP_NAME,
+  storageBucket: `${FIREBASE_APP_NAME}.appspot.com`,
+  messagingSenderId: FIREBASE_SENDER_ID,
+  appId: FIREBASE_APP_ID,
 }
 
 // Initialize Firebase
